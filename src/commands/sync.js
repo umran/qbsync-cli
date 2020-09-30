ed3ef1e08e42e6cc3ea2d3e3ecd625ec473a82f1
Umran Hussein <umran.hussein@gmail.com>
2020-09-30 01:40:51 -0700

    initial commit

---
+++ src/commands/sync.js
@@ -0,0 +1,67 @@
+const { Command, flags } = require("@oclif/command")
+const { cli } = require("cli-ux")
+const chalk = require("chalk")
+const {
+    QuickBooksEngine,
+    ShopifyEngine,
+    validateProductVariants,
+    logProductVariantValidationResults,
+    parseProduct
+} = require("quickbooks-sync")
+const { shopify: shopify_config, quickbooks: quickbooks_config } = require("../../config")
+
+const sb = new ShopifyEngine(shopify_config)
+const qb = new QuickBooksEngine(quickbooks_config)
+
+class SyncCommand extends Command {
+    async run() {
+        cli.action.start(chalk.gray.bold("    fetching product variants"))
+        const variants = await sb.getAllProductVariants()
+        cli.action.stop(chalk.green.bold("done"))
+
+        cli.action.start(chalk.gray.bold("    validating product variants"))
+        const { ok, results } = validateProductVariants(variants)
+        cli.action.stop(chalk.green.bold("done"))
+
+        if (!ok) {
+            console.log("\n")
+            logProductVariantValidationResults({ ok, results })
+            return
+        }
+
+        // since there are no validation errors we can proceed with syncing the products
+        cli.action.start(chalk.gray.bold("    refreshing access token"))
+        await qb.refreshAccessToken()
+        cli.action.stop(chalk.green.bold("done"))
+
+        // create a progress bar
+        const progress = cli.progress({
+            format: `    ${chalk.gray.bold("syncing product variants | ")}${chalk.greenBright.bold("{bar}")}${chalk.gray.bold(" | {value}/{total} items")}`,
+            barCompleteChar: '\u2588',
+            barIncompleteChar: '\u2591',
+        })
+        progress.start(variants.length, 0)
+        for (const [index, variant] of variants.entries()) {
+            const product = parseProduct(variant)
+            await qb.syncProduct(product)
+            progress.increment(1)
+        }
+        progress.stop()
+        console.log("\n")
+        console.log(chalk.green.bold("    all product variants synced successfully"))
+        console.log("\n")
+    }
+
+    async catch(err) {
+        console.error(err)
+    }
+}
+
+SyncCommand.description = `Syncs Shopify product variants with Quickbooks
+This command retrieves all product variants from Shopify, validates them
+then syncs them with Quickbooks
+`
+
+SyncCommand.flags = {}
+
+module.exports = SyncCommand
---
+++ src/commands/validate.js
@@ -0,0 +1,35 @@
+const { Command, flags } = require("@oclif/command")
+const { cli } = require("cli-ux")
+const chalk = require("chalk")
+const {
+    ShopifyEngine,
+    validateProductVariants,
+    logProductVariantValidationResults
+} = require("quickbooks-sync")
+const { shopify } = require("../../config")
+
+const sb = new ShopifyEngine(shopify)
+
+class ValidateCommand extends Command {
+    async run() {
+        cli.action.start(chalk.gray.bold("    fetching product variants"))
+        const variants = await sb.getAllProductVariants()
+        cli.action.stop(chalk.green.bold("done"))
+
+        cli.action.start(chalk.gray.bold("    validating product variants"))
+        const { ok, results } = validateProductVariants(variants)
+        cli.action.stop(chalk.green.bold("done"))
+
+        console.log("\n")
+        logProductVariantValidationResults({ ok, results })
+    }
+}
+
+ValidateCommand.description = `Validates Shopify product variants
+This command retrieves all product variants from Shopify and checks for any errors
+such as the existence of duplicate skus, barcodes and fully qualified product names
+`
+
+ValidateCommand.flags = {}
+
+module.exports = ValidateCommand
